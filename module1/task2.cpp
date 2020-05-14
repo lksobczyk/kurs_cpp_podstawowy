e8ba498f68934e5f0e8e7935bbdb02d5b04882d3
Lukasz <lksobczyk@wp.pl>
2020-05-14 20:05:23 +0200

    Update task2.cpp

--- module1/task2.cpp
+++ module1/task2.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
 // Write your function here
+int max(int first, int second, int third)
+{
+    return (first > second) ?
+        ((first > third) ? first : third) :
+        ((second > third) ? second : third);    
+}
 
 int main() {
     std::cout << "max (1, 2, 3): " << max(1, 2, 3) << "\n";
