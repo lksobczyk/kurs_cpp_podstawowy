55731e58d347d00834a57ce2481156523be5850b
Lukasz <lksobczyk@wp.pl>
2020-05-14 20:33:19 +0200

    Update task3.cpp

--- module1/task3.cpp
+++ module1/task3.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
 // Write your function here
+void printString(const std::string& text, size_t count)
+{
+    for (size_t k = 0; k < count; ++k)
+    {
+        std::cout << text << " ";
+    }
+}
 
 int main() {
     printString("Hello", 5);
