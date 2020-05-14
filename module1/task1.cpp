6296506151fb77ae80feef5b965b975b77c6d059
Lukasz <lksobczyk@wp.pl>
2020-05-14 19:45:18 +0200

    Update task1.cpp

--- module1/task1.cpp
+++ module1/task1.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
 // Write missing function here
+int multiply(int left, int right) {
+    return left * right;
+}
 
 int main() {
     std::cout << "4 * 5: " << multiply(4, 5) << "\n";
