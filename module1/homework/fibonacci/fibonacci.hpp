e67480169c85d747f3fc7e15e23948d3ded1b4af
Lukasz <lksobczyk@wp.pl>
2020-05-17 07:08:13 +0200

    the second cohomology

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,37 @@
 #pragma once
 
-int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+
+int fibonacci_iterative(int sequence)
+{
+	if (sequence < 1)
+		return 0;
+
+	if (sequence == 1)
+		return 1;
+
+
+	int first = 0, second = 1;
+	int result = 0;
+
+	for(int k = 2; k <= sequence; ++k)
+	{
+		result = first + second;
+		first = second;
+		second = result;
+	}
+
+	return result;
 }
 
-int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+int fibonacci_recursive(int sequence)
+{
+	if (sequence < 1)
+		return 0;
+
+	if (sequence == 1)
+		return 1;
+
+
+	return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
 }
+
