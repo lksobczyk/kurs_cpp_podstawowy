a6ce716bdd6293a8797c5d1cc38e5cac27bcd3f1
KrzysztofJ85 <krzysztof.jucha@gmail.com>
2020-05-23 14:24:47 +0200

    Homework Podstawy C++ #2 - maxOfVector Author: @KrzysztofJ85 first element (int max) added + test

--- module2/homework/maxOfVector/maxOfVector.hpp
+++ module2/homework/maxOfVector/maxOfVector.hpp
@@ -4,10 +4,13 @@
 
 int maxOfVector(const std::vector<int> &vec)
 {
+    int max = vec.front();
+    std::cout << max << '\n' << '\n';
     for (auto it = vec.begin(); it != vec.end(); ++it)
     {
         std::cout << *it << '\n';
     }
+    std::cout << '\n';
 
     return {};
 }
