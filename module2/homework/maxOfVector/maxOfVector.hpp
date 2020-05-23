406c6df65813fed9e72a22049b1d281ac86d698e
KrzysztofJ85 <krzysztof.jucha@gmail.com>
2020-05-23 15:10:21 +0200

    Homework Podstawy C++ #2 - maxOfVector Author: @KrzysztofJ85 loop test without "auto" keyword

--- module2/homework/maxOfVector/maxOfVector.hpp
+++ module2/homework/maxOfVector/maxOfVector.hpp
@@ -5,7 +5,7 @@
 int maxOfVector(const std::vector<int> &vec)
 {
     int max = vec.front();
-    for (auto it = vec.begin(); it != vec.end(); ++it)
+    for (std::vector<int>::const_iterator it = vec.begin(); it != vec.end(); ++it)
     {
         if (*it > max)
         {
