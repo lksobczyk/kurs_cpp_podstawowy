d69f342d42031c2963c5011bdfaaa98c6c657446
Lukasz <lksobczyk@wp.pl>
2020-05-24 20:07:12 +0200

    the greater evil

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,11 +1,18 @@
 #pragma once
 
-int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+int NWD(int lhs, int rhs)
+{
+	while (rhs != 0)
+	{
+		int temp = rhs;
+		rhs = lhs % rhs;
+		lhs = temp;
+	}
+
+	return lhs;
 }
 
-int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+int NWW(int lhs, int rhs)
+{
+	return (lhs * rhs) / NWD(lhs, rhs);
 }
