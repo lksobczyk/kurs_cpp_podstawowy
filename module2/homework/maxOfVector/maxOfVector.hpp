6e182751d595ad220cc60f4ab6104d6a36d390e3
KrzysztofJ85 <50273916+KrzysztofJ85@users.noreply.github.com>
2020-05-26 07:55:39 +0200

    check for empty vector added

--- module2/homework/maxOfVector/maxOfVector.hpp
+++ module2/homework/maxOfVector/maxOfVector.hpp
@@ -3,6 +3,10 @@
 
 int maxOfVector(const std::vector<int> &vec)
 {
+    if (vec.empty())
+    {
+        return 0;
+    }
     int maxValue = vec.front();
     for (auto &vectorElement : vec)
     {
