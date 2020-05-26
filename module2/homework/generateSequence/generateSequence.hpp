90b7d552b7810c2a1ee0e32aa5901b67aa52d230
KrzysztofJ85 <50273916+KrzysztofJ85@users.noreply.github.com>
2020-05-26 07:51:44 +0200

    vec.reserve(count) added

--- module2/homework/generateSequence/generateSequence.hpp
+++ module2/homework/generateSequence/generateSequence.hpp
@@ -4,6 +4,7 @@
 std::vector<int> generateSequence(int count, int step)
 {
     std::vector<int> vec;
+    vec.reserve(count);
     int sequenceElement = 0;
     for (int i = 0; i < count; ++i)
     {
